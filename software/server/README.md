560ca4284b0449d2a4ed29b11b52c4058a74ff0c
Ubuntu <marin.thiercelin@gmail.com>
2020-04-04 21:49:21 +0000

    added dummy images and readme

--- software/server/README.md
+++ software/server/README.md
@@ -1,1 +1,12 @@
 # Procam server
+
+## Installation:
+- install nodejs
+- install npm 
+- run `npm install`
+
+## Launch the web server:
+- run `nodejs src/server.js`
+
+## Configuration:
+- change the configuration in `src/config.json`
--- software/server/src/server.js
+++ software/server/src/server.js
@@ -2,61 +2,16 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const config = require('config.json');
+const config = require(path.join(__dirname,'config.json'));
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/../images')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
   console.log('page');
 })
-
-function returnImage(image_path){
-  return (req, res) => {
-    const stat = fs.statSync(video_path);
-    const fileSize = stat.size;
-    const range = req.headers.range;
-
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0], 10);
-      const end = parts[1]
-        ? parseInt(parts[1], 10)
-        : fileSize-1;
-
-      if(start >= fileSize) {
-        res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
-        return;
-      }
-      
-      const chunksize = (end-start)+1;
-      const file = fs.createReadStream(video_path, {start, end});
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-      };
-
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      };
-      res.writeHead(200, head);
-      fs.createReadStream(video_path).pipe(res);
-    }
-  }
-};
-
-const thermal_image_path = path.resolve(__dirname+'/../images/thermal_sample.jpeg');
-app.get('/thermal', returnImage(thermal_image_path));
-
-const visible_image_path = path.resolve(__dirname+'/../images/visible_sample.jpeg');
-app.get('/visible', returnImage(visible_image_path));
 
 app.listen(config.network.port, function () {
   console.log(`Listening on port ${config.network.port}!`);
