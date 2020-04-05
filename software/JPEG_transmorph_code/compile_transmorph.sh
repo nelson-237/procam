cb9f065aeedebb395818ba5861b11eccd71be468
Marin Thiercelin <marin.thiercelin@epfl.ch>
2020-04-05 23:39:43 +0200

    added -lm in compilation

--- software/JPEG_transmorph_code/compile_transmorph.sh
+++ software/JPEG_transmorph_code/compile_transmorph.sh
@@ -6,7 +6,7 @@
 # gcc -O2  -I.   -c -o jpegremorph.o jpegremorph.c
 # gcc  -o jpegremorph jpegremorph.o rdswitch.o cdjpeg.o transupp.o libjpeg.a
 
-gcc -O2 -I. -c -o jptrsmorph.o jptrsmorph.c
+gcc -O2 -I. -c -o jptrsmorph.o jptrsmorph.c -lm
 echo "Compiling 1:  gcc -O2 -I. -c -o jptrsmorph.o jptrsmorph.c"
-gcc -o jptrsmorph jptrsmorph.o src/rdswitch.o src/cdjpeg.o src/transupp.o src/libjpeg.a
+gcc -o jptrsmorph jptrsmorph.o src/rdswitch.o src/cdjpeg.o src/transupp.o src/libjpeg.a -lm
 echo "Compiling 2:  gcc -o jptrsmorph jptrsmorph.o src/rdswitch.o src/cdjpeg.o src/transupp.o src/libjpeg.a"
