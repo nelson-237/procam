27861ed6a06237dc7ed512d2537cbeaa2e6eadcd
marin <marin.thiercelin@gmail.com>
2020-04-05 14:00:41 +0200

    started image receiver

---
+++ software/server/src/image_receiver.js
@@ -0,0 +1,28 @@
+var net = require('net');
+var fs = require('fs');
+var buffer = require('buffer');
+const config = require(path.join(__dirname,'config.json'));
+
+module.exports = function(){
+    const port = config.image_receiver.network.port;
+    const host = config.image_receiver.network.host;
+    var server = net.createServer((conn) => {
+        console.log('image transfer connected');
+        conn.on('data', (data) => {
+            console.log('data received');
+            console.log('data is: \n' + data);
+        });        
+    });
+
+    server.listen(
+        port, 
+        host,
+        () => {
+            console.log('server bound to ' + port + '\n');
+            server.on('connection', () => {
+                console.log('connection made...\n')
+            });
+        }
+    );
+}
+
