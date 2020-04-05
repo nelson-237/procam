78cad105f078feebb3e4fae414186abbf6e41485
marin <marin.thiercelin@gmail.com>
2020-04-05 02:20:51 +0200

    made styling

--- software/server/src/server.js
+++ software/server/src/server.js
@@ -8,9 +8,10 @@ const config = require(path.join(__dirname,'config.json'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.resolve(__dirname+'/../images')));
 
-function returnImage(image_path){
+function returnImage(image_dir, image_name){
+  const image_path = path.join(image_dir, image_name);
   return (req, res) => {
-    console.debug(`image: ${path.basename(image_path)}`);
+    console.debug(`image: ${image_name}`);
     if (! fs.existsSync(image_path)) {
       res.status(403).end('Forbidden');
     }
@@ -28,10 +29,9 @@ function returnImage(image_path){
 }
 
 const image_dir = path.resolve(__dirname+'/../images/');
-const thermal_image_path = path.resolve(image_dir+'/thermal.jpeg');
-app.get('/image/thermal', returnImage(thermal_image_path));
-const visible_image_path = path.resolve(image_dir+'/visible.jpeg');
-app.get('/image/visible', returnImage(visible_image_path));
+app.get('/image/stream', returnImage(image_dir, 'stream.jpeg'));
+app.get('/image/thermal', returnImage(image_dir, 'thermal.jpeg'));
+app.get('/image/visible', returnImage(image_dir, 'visible.jpeg'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
