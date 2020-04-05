a8f15c49059320c4df57b33de6d0551b9da2931e
Marin Thiercelin <marin.thiercelin@epfl.ch>
2020-04-05 23:47:46 +0200

    added module 3 in script

--- software/raspberry/start_streaming.sh
+++ software/raspberry/start_streaming.sh
@@ -1,17 +1,23 @@
-CAMERA_ID=1 #modify to camera id
-WAIT_TIME=0.1s
-SERVER_HOST=192.168.0.33 #modify to server ip
-SERVER_PORT=8080
-mkdir -p build
+CAMERA_ID=1 #camera id
+WAIT_TIME=0.1s #sampling period
+SERVER_HOST=192.168.0.33 #server ip
+SERVER_PORT=8080 #server port
+JPTRSMORPH_BIN=../JPEG_transmorph_code/jptrsmorph #path to the jptrsmorph binary
+
+BUILD_DIR=build
+mkdir -p $BUILD_DIR
+VISIBLE_OUTPUT=$BUILD_DIR/visible.jpeg
+IR_OUTPUT=$BUILD_DIR/thermal.jpeg
+
 while :
 do
-  python3 module1_create_visible_jpeg.py build/visible.jpeg --simulation;
-  python3 module2_create_ir_jpeg.py build/thermal.jpeg --simulation;
+  python3 module1_create_visible_jpeg.py $VISIBLE_OUTPUT --simulation;
+  python3 module2_create_ir_jpeg.py $IR_OUTPUT --simulation;
   current_time=$(date "+%Y.%m.%d-%H.%M.%S");
-  file_name=$CAMERA_ID-$current_time.jpeg;
-#   ./jptrsmorph -morph build/visible.jpeg build/thermal.jpeg build/$file_name.jpeg;
-  cp build/thermal.jpeg build/$file_name;
-  python3 module4_send_image_to_server.py $SERVER_HOST $SERVER_PORT build/$file_name;
+  file_name=$BUILD_DIR/$CAMERA_ID-$current_time.jpeg;
+  $JPTRSMORPH_BIN -morph $VISIBLE_OUTPUT $IR_OUTPUT $file_name;
+  # cp build/thermal.jpeg $file_name;
+  python3 module4_send_image_to_server.py $SERVER_HOST $SERVER_PORT $file_name;
   rm build/*.jpeg;
   sleep $WAIT_TIME;
 done
